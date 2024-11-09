925f6a3ee99b46de8a9fcca5e332244989b48f69
Lucas Ergh <lucasergh@gmail.com>
2024-11-08 22:41:55 -0500

    Created Sprites files

---
+++ include/Sprites.h
@@ -0,0 +1,15 @@
+#ifndef SPRITES_H
+#define SPRITES_H
+
+#include "BaseWindow.h"
+
+// We wil probably need windows animation in the future
+// https://learn.microsoft.com/en-us/windows/win32/uianimation/windows-animation-developer-guide
+// It uses the COM library
+     
+
+class Sprites : public BaseWindow<Sprites> {
+    
+};
+
+#endif
